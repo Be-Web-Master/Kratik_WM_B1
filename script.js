5028a7ac6a97a2d78a690bd1a4508e37fa8eced6
kratiks26 <kratiks26@gmail.com>
2023-08-22 16:48:33 +0530

    unsolved -----

---
+++ script.js
@@ -0,0 +1,47 @@
+//  let unsplited=  [6,8,11,9,3,5,15,1];
+//  let splited=(arr.map(x=>[x]));
+
+//  function mergesort(arr1, arr2){
+//     let arr =[ ];
+
+//     for(let i =0; i< arr1.length; i++){
+//         if(arr1[i]> arr2[i]){
+//             arr.push(arr2[i])
+//         }
+//         else{
+//             arr.push(arr1[i])
+//         }
+
+//     }
+
+//  };
+//  console.log(mergesort(unsplited, splited));
+
+
+
+// function ms(){return []}
+
+// function newArrSorted(arr){
+//     let arr =[];
+//     let first= 0, sec=1;
+//     while(first <= arr.length && sec <= arr.length)
+// }
+
+
+let arr =[6,8,11,9,3,5,15,1];
+let result=[];
+for( let i = 0; i<arr.length; i+=2){
+    let j;
+    if(i === arr.length-1){
+
+    }
+    j=i+1;
+
+    console.log({i:arr[i], j:arr[j]})
+
+    // result.push([arr[i],arr[j]]);
+
+    
+}
+console.log(result);
+
