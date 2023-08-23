30d2cdd5f12ed94404cd11e43391ddf14305cf13
kratiks26 <kratiks26@gmail.com>
2023-08-23 14:16:11 +0530

    task1 completed

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ let mybutn= document.getElementById('mybutton')
 //     {funtodo('https://jsonplaceholder.typicode.com/todos')}
 // );
 
-const funtodo = async (url)=>{
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
-};
+// const funtodo = async (url)=>{
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     console.log(data)
+// };
 
 
 
-// mybutn.addEventListener('click', ()=>{
-//     fetch("https://jsonplaceholder.typicode.com/todos");
-//     .then((response) => response.json())
-//     .then((data)=> console.log(data) )
 
-// })
 
-// comapleit 
+mybutn.addEventListener( 'click', () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+})
