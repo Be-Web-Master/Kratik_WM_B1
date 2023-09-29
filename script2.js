aa3c78fc8577a664bf3865bc4b4d0a0f40eac974
kratiks26 <kratiks26@gmail.com>
2023-09-29 11:48:31 +0530

    Q3

---
+++ script2.js
@@ -0,0 +1,4 @@
+// Find the greatest dublicate number among dublicates
+// function findMaxDublicate(arr) - this function will return the max dublicate number of an array
+// const arr = [11,1,12,2,5,5,6,11,14,11]
+// output - 11 - 11 is dublicate and the biggest number among all dublicates
---
+++ script3.js
@@ -0,0 +1,28 @@
+// Find the average score of a player.
+// function getAverage(arr) - this function will return average score of a player
+// arr - an array contains the balls played by the player and the runs, this array also include "wide" ball or "Wide" ball, you don't have to include this ball in players score
+// the index of the array is a ball
+// and the value at the index are the runs
+
+const arr = [4,2,1,4,6,"wide",4,2,4,"Wide", 4,6,"wide",6,6]
+// output - 4.083s
+
+function getAverage(arr){
+
+    let arr2 = [];
+    let output = 0;
+
+    for(let i = 0 ; i< arr.length;  i++){
+         
+        if( typeof(arr[i])=== typeof(0)){
+            arr2.push(arr[i])
+        }
+
+    }
+    return arr2;
+
+}
+
+
+console.log(getAverage(arr));
+
