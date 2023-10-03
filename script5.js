5cf3591ffdc17971d1d443bc195ab989a216fcc1
kratiks26 <kratiks26@gmail.com>
2023-10-03 11:18:31 +0530

    day-5

---
+++ script5.js
@@ -0,0 +1,31 @@
+
+// Reverse all the words of a string - do not use built-in method
+// function reverseWords(string) - this function will return an string having reversed words
+// const string - "webMaster is Best"
+// output - "retsaMbew si tesB"
+
+const string = "webMaster is Best";
+
+
+
+function reverseWords(string){
+
+    let output = "";
+    let str2= string.split(" ") ;
+    for(let i = 0; i<str2.length; i++){
+
+        // for(let j =str2[i].length-1 ; j<=0; i--){
+        //     
+        // }
+        output = str2[];
+
+    }
+
+    return output;
+}
+
+console.log(reverseWords(string));
+
+
+
+
