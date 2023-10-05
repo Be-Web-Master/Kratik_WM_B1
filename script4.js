b9c43c3171ea11e136b81bd85039149374f60790
kratiks26 <kratiks26@gmail.com>
2023-10-05 12:08:59 +0530

    Q4

--- script4.js
+++ script4.js
@@ -13,6 +13,9 @@ function findRunningSum(arr){
 
     let output = [];
     for(let i = 0; i< arr.length; i++){
+        for( let j = 1 ; j<arr.length; j++){
+            output.push(arr[i]+arr[j])
+        }
         
     }
 
---
+++ script5.js
@@ -0,0 +1,22 @@
+// Implement Selection Sort - ascending order
+// const arr = [11, 20, 3, 14, 50, 9]
+// function selectionSort(arr) - this function will return array of sorted elements in ascending order
+// output = [3,9,11,14,20,50]
+
+
+
+const arr = [11, 20, 3, 14, 50, 9];
+
+
+function selectionSort(arr){
+    let output = [];
+    // let output2 = [];
+    for(let i=0; i<arr.length;i++){
+        for(let j=1; j<arr.length;j++){
+             if(arr[i] < arr[j]){
+                arr.push
+             }
+
+        }
+    }
+}
