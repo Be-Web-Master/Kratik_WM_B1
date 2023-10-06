569cd661fab43019173d8ece4848e71e9cddfede
kratiks26 <kratiks26@gmail.com>
2023-10-06 11:52:48 +0530

    Q3

---
+++ script3.js
@@ -0,0 +1,16 @@
+// Find the square root of a given number - do not use built in method
+// const num = 16
+// output - 4
+// function getSqrt(num) - this function will return square root of a number
+
+
+const num = 16
+
+
+function getSqrt(num){
+
+    let output =0;
+    output = num/2;
+}
+
+console.log(getSqrt(num));
