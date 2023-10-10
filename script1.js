8cb29a49bee93da91c46a4ffa15339c36267eae5
kratiks26 <kratiks26@gmail.com>
2023-10-10 11:21:04 +0530

    Q1

---
+++ script1.js
@@ -0,0 +1,23 @@
+// Easy - Flat the array - do not use built in method
+// const arr = [ [1, 2], [3,4], [5,6] ]
+// output - [1,2,3,4,5,6]
+// function flatArray(arr) - this function will return flatten array
+
+const arr = [ [1, 2], [3,4], [5,6] ]
+
+function flatArray(arr){
+    let output=[];
+
+    for(let i = 0; i< arr.length; i++){
+        for(let j = 0; j<arr[i].length; j++){
+
+            output.push(arr[i][j]);
+
+        }
+    }
+    return output;
+};
+
+
+console.log(flatArray(arr));
+
